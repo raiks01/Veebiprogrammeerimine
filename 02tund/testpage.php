fd336c03394a7273a034025955ca5cc504c800c5
Rainer Halanurm <raiks01@users.noreplay.github.com>
2019-09-10 11:09:15 +0300

    esimene üleslaadimine

---
+++ 02tund/testpage.php
@@ -0,0 +1,22 @@
+<?php
+  $userName = "Rainer ya boi Halanurm";
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>
+  <?php
+    echo $userName;
+  ?>
+  test website</title>
+
+</head>
+<body>
+  <?php
+  echo "<h1>" .$userName .", veebiprogrammeerimine </h1>";
+  ?>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiselt võetavat sisu! Eelmine lause ei ole tõene!! </p>
+  <p>ERROR 404 NOT FOUND </p>
+  <p><b style="color:#00FF00">The End Is Near! Be ready to die in few days!</b></p></body>
+</html>
