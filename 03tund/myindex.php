4d6a1ddabe45c3f1975f2c9763252a0b95ad5932
Rainer Halanurm <raiks01@users.noreplay.github.com>
2019-09-17 11:29:47 +0300

    fotode kataloogide lugemine, juhuslik pilt

---
+++ 03tund/myindex.php
@@ -0,0 +1,23 @@
+<?php
+  //echo "Hei!";
+  phpversion();
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>Rainer test website</title>
+
+</head>
+<body>
+  <h1><b style="color:#0606F5">Rainer Halanurm, veebiprogrammeerimine</b></h1>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiselt võetavat sisu! Eelmine lause ei ole tõene!! </p>
+  <p><b style="color:#F80303">ERROR 404 NOT FOUND </b></p>
+  <p><b style="color:#00FF00">The End Is Near! Be ready to die in few days!</b></p>
+  <img src="https://media1.tenor.com/images/de1678a4a0d3ca9264a17339e87dfb5e/tenor.gif?itemid=7204975" width="833" height="625.6755555555555" alt="The End Is Near GIF - TheSimpsons HomerSimpson TheEndIsNear GIFs" style="max-width: 833px; background-color: rgb(63, 63, 63);">
+  <hr>
+  </p>PHP serveri konfiguratsiooni infot näeb <a href="serverinfo.php">siin</a>.</p> 
+  <p>Minu esimene programmeeritud leht on <a href="testpage.php">siin</a>.</p>
+  </p>Siia võid minna ainult omal vastutusel kui sa oled 18+ ja sa pead olema täiesti kindel endas et sa tahad siia tulla! Kui oled valmis siis vajuta <a href="https://www.youtube.com/watch?v=kV6mx-yHGJs">SIIA</a>.</p>
+  </body>
+</html>
